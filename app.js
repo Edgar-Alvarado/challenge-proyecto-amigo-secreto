a01c34f354c0c56dddd9d8d8ed90275f9a34c5bb
Edgar Alvarado <l.edgaralvarado1gmail.com>
2025-07-30 16:44:26 -0600

    Primeros cambios

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//variables
+let nombres = [];
+function nombreArray(){
+    let ingresarNombre = nombres.push('Jorge');
+    console.log(nombres);
+}
