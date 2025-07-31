fe9d66d5167b372134b6e87347bf587c1fd530e5
Edgar Alvarado <l.edgaralvarado1gmail.com>
2025-07-31 11:39:09 -0600

    Nuevas funciones añadidas

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 //variables
 let nombres = [];
-function nombreArray(){
-    let ingresarNombre = nombres.push('Jorge');
-    console.log(nombres);
+//Funcion para añadir nombres a la lista o Array
+function agregarAmigo(){
+    let amigo = document.getElementById("amigo").value;
+    if(amigo !== ""){
+        nombres.push(amigo);
+        actualizarLista();
+        document.getElementById("amigo").value = "";
+    }
+    else{
+        alert('⚠️ La caja de texto está vacía. Por favor, escribe un nombre.');
+    }
+}
+
+//Escucha la tecla enter en el input
+//Selecciona el input del html, escucha cuando presionamos una tecla.
+document.getElementById("amigo").addEventListener("keydown", function(event){
+    if(event.key == "Enter"){ //Verifica si la tecla fue enter.
+        agregarAmigo(); //llama a la funcion para añadir el nombre.
+    }
+});
+
+// Función para actualizar la lista de nombres
+function actualizarLista(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    nombres.forEach(amigo =>{
+        lista.innerHTML += `<li>${amigo}</li>`;
+    });
 }
